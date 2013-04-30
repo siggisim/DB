94bb4c6e6aab092d8ff72380b63cf4144eb50d94
siggisim <siggisim@gmail.com>
2013-04-30 15:47:50 -0300

    Update DB.config.php

--- DB.config.php
+++ DB.config.php
@@ -1,8 +1,8 @@
 <?
   require_once("DB.class.php");
-  $config['host']     = 'HOST';
+  $config['host']      = 'HOST';
   $config['username']  = 'USERNAME';
-  $config['password'] = 'PASSWORD';
+  $config['password']  = 'PASSWORD';
   $config['database']  = 'DATABASE';
   $db = new DB($config);    
 ?>
